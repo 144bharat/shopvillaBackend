6f111b7fbda8cf11eb1d6f51e7d065cbe8b144ad
144bharat <144bharatgautam@gmail.com>
2025-03-04 23:01:39 +0530

    Changes in server.js

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const jsonServer = require('json-server');
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('database.json');
+// const middlewares = jsonServer.defaults();
+
+// server.use(middlewares);
+// server.use(router);
+
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => {
+//   console.log(`JSON Server is running on port ${port}`);
+// });
+
+// module.exports = server;
+
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('database.json');
+const router = jsonServer.router("database.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
-
-module.exports = server;
+module.exports = server; // Remove manual port listening
